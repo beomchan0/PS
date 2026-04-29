285c5b606fbaedc4276dd364950f29a7279be7af
Beomchan Kim <goldensnitch6750@gmail.com>
2026-04-29 16:08:01 +0900

    Title: 변수 선언하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260429/변수 선언하기/declaring-variables.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    char b;
+    a=3;
+    b='C';
+    cout << a << "\n" << b;
+    return 0;
+}
