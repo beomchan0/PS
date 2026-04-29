e850f253d9393ded62bf7522a08f017b419bae4d
Beomchan Kim <goldensnitch6750@gmail.com>
2026-04-29 16:29:10 +0900

    Title: 달에서 무게 구하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260429/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double a = 13;
+    double b = 0.165;
+
+    cout << fixed;
+    cout.precision(6);
+
+    cout << "13 * 0.165000 = " << a*b; 
+    return 0;
+}
