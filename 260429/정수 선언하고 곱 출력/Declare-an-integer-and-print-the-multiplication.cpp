dbafcb1435e0301643c1c1cb570464375b28b2a3
Beomchan Kim <goldensnitch6750@gmail.com>
2026-04-29 16:10:39 +0900

    Title: 정수 선언하고 곱 출력, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260429/정수 선언하고 곱 출력/Declare-an-integer-and-print-the-multiplication.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    a=26;
+    b=5;
+    cout << a << " * " << b << " = " << a*b;
+    return 0;
+}
