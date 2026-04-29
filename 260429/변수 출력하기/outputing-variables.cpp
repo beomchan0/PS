9092df4b74dcf570bb3df4ec793d6720024402d4
Beomchan Kim <goldensnitch6750@gmail.com>
2026-04-29 16:16:33 +0900

    Title: 변수 출력하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260429/변수 출력하기/outputing-variables.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    char b;
+    a = 3;
+    b = 'C';
+
+    cout << a << "..." << b;
+    return 0;
+}
