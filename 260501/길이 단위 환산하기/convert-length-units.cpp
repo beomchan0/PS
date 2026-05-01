0cc801903a8999af054a33f782958315feba2df7
Beomchan Kim <goldensnitch6750@gmail.com>
2026-05-01 23:12:05 +0900

    [Passed] 길이 단위 환산하기 (8ms, 0MB)

---
+++ 260501/길이 단위 환산하기/convert-length-units.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double a;
+    cin >> a;
+    cout << fixed;
+    cout.precision(1);
+
+    cout << a*30.48;
+
+    return 0;
+}
