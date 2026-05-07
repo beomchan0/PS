e69edc86db57f4c3e17feb536ab91bf33ac2df87
Beomchan Kim <goldensnitch6750@gmail.com>
2026-05-07 16:41:15 +0900

    [Passed] 날짜 변경하여 출력 2 (8ms, 0MB)

---
+++ 260507/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b,c;
+    char d,e;
+    cin >> a >> d >> b >> e>> c;
+    cout << c << "." << a << "." << b;
+    return 0;
+}
