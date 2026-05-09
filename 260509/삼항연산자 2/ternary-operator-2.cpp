ef34e499faa6cc54c7da5790b8071d1a8c8a369c
Beomchan Kim <goldensnitch6750@gmail.com>
2026-05-09 23:18:42 +0900

    [Passed] 삼항연산자 2 (8ms, 0MB)

---
+++ 260509/삼항연산자 2/ternary-operator-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin >> a;
+    char c;
+    c = a==1 ? 't' : 'f';
+    cout << c;
+    return 0;
+}
