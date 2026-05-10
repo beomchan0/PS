70e8c646d41dac1f68c766caa7b06d649fe894bf
Beomchan Kim <goldensnitch6750@gmail.com>
2026-05-10 21:13:09 +0900

    [Passed] 출석 부르기 (8ms, 0MB)

---
+++ 260510/출석 부르기/calling-attendance.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin >> a;
+    if(a==1){
+        cout << "John\n";
+    }else if(a==2){
+        cout << "Tom\n";
+    }else if(a==3){
+        cout << "Paul\n";
+    }else{
+        cout << "Vacancy\n";
+    }
+    return 0;
+}
