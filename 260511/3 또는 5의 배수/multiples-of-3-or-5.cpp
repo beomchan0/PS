dac4e9e6aea5ca07a21aad9fe1190ad1db675d7e
Beomchan Kim <goldensnitch6750@gmail.com>
2026-05-11 02:16:13 +0900

    [Passed] 3 또는 5의 배수 (8ms, 0MB)

---
+++ 260511/3 또는 5의 배수/multiples-of-3-or-5.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin >> a;
+    if(a%3==0){
+        cout << "YES\n";
+    }else{
+        cout << "NO\n";
+    }
+
+    if(a%5==0){
+        cout << "YES\n";
+    }else{
+        cout << "NO\n";
+    }
+    return 0;
+}
