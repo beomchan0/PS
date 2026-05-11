7e2ccfc7578fe579e8aaec26459393f0d214ba0e
Beomchan Kim <goldensnitch6750@gmail.com>
2026-05-11 15:33:47 +0900

    [Passed] 범위 밖의 수 (8ms, 0MB)

---
+++ 260511/범위 밖의 수/number-out-of-range.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin >> a;
+    if(a<10 || a>20){
+        cout << "yes\n";
+    }else{
+        cout << "no\n";
+    }
+    return 0;
+}
