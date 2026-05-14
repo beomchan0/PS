9c487b8a54166c66f2f8c48222303f1c262f14c5
Beomchan Kim <goldensnitch6750@gmail.com>
2026-05-14 14:24:08 +0900

    [Passed] 일 수 구하기 (8ms, 0MB)

---
+++ 260514/일 수 구하기/number-of-days-in-month.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    if(n<8){
+        if(n==2){
+            cout << "28";
+        }else if(n%2==0){
+            cout << "30";
+        }else{
+            cout << "31";
+        }
+    }else{
+        if(n%2==0){
+            cout << "31";
+        }else{
+            cout << "30";
+        }
+    }
+    return 0;
+}
