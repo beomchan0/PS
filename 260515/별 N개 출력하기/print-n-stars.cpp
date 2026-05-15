1ade1d365d12833be9056a244692305e62f2f7bf
Beomchan Kim <goldensnitch6750@gmail.com>
2026-05-15 19:39:04 +0900

    [Passed] 별 N개 출력하기 (8ms, 0MB)

---
+++ 260515/별 N개 출력하기/print-n-stars.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N;
+    cin >> N;
+    while(N>0){
+        cout << "*\n";
+        N--;
+    }
+    return 0;
+}
