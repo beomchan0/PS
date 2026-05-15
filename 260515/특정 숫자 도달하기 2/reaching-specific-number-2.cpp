ea0a882add2e2186a7291f17a37c489e76bb0afa
Beomchan Kim <goldensnitch6750@gmail.com>
2026-05-15 19:19:15 +0900

    [Passed] 특정 숫자 도달하기 2 (8ms, 0MB)

---
+++ 260515/특정 숫자 도달하기 2/reaching-specific-number-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N;
+    cin >> N;
+    for(int i=N; i>0; i--){
+        cout << i << " ";
+    }
+    return 0;
+}
