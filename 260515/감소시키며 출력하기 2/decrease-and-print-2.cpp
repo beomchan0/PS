b7a47db9c01289602b666551b20767159ddd12f6
Beomchan Kim <goldensnitch6750@gmail.com>
2026-05-15 20:02:26 +0900

    [Passed] 감소시키며 출력하기 2 (8ms, 0MB)

---
+++ 260515/감소시키며 출력하기 2/decrease-and-print-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int i=26;
+    while(i>=10){
+        cout << i-- << " ";
+    }
+    return 0;
+}
