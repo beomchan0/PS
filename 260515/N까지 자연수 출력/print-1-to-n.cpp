6eb98505cfae63e05ad9136854af2d1030f35ee6
Beomchan Kim <goldensnitch6750@gmail.com>
2026-05-15 19:36:59 +0900

    [Passed] N까지 자연수 출력 (8ms, 0MB)

---
+++ 260515/N까지 자연수 출력/print-1-to-n.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N;
+    cin >> N;
+    int i=1;
+    while(i<=N){
+        cout << i++ << " ";
+    }
+    return 0;
+}
