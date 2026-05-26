fd7db9307c4baf5d6a09c196067bb17a2f6460a4
Beomchan Kim <goldensnitch6750@gmail.com>
2026-05-26 17:26:01 +0900

    [Passed] 20대의 평균 나이 (2ms, 0MB)

---
+++ trail1/20대의 평균 나이/average-age-of-20.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int sum=0;
+    int cnt=0;
+
+    while(1){
+        int a; 
+        cin >> a;
+        if(a>=30||a<20){
+            break;
+        }
+
+        sum += a;
+        cnt++;
+    }
+
+    cout << fixed;
+    cout.precision(2);
+    cout << (double)sum/cnt;
+    return 0;
+}
