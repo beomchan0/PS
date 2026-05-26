6994ad46f7736eabf540ae896face9e3fe7e938b
Beomchan Kim <goldensnitch6750@gmail.com>
2026-05-26 17:01:58 +0900

    [Passed] 0 나올때까지 출력 (2ms, 0MB)

---
+++ trail1/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    while(1){
+        int a;
+        cin >> a;
+
+        if(a==0){
+            break;
+        }
+
+        cout << a << "\n";
+    }
+    return 0;
+}
