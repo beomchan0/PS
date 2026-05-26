32638d26b0c5f4d0d265cd6e421cde883c26be40
Beomchan Kim <goldensnitch6750@gmail.com>
2026-05-26 15:46:23 +0900

    [Passed] 청소당번 정하기 (2ms, 0MB)

---
+++ trail1/청소당번 정하기/cleaning-numbering.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int cnt1=0;
+    int cnt2=0;
+    int cnt3=0;
+
+    int n;
+    cin >> n;
+
+    for(int i=1; i<=n; i++){
+        if(i%12==0){
+            cnt3++;
+        }else if(i%3==0){
+            cnt2++;
+        }else if(i%2==0){
+            cnt1++;
+        }
+
+        
+    }
+
+    cout << cnt1 << " " << cnt2 << " " << cnt3;
+    return 0;
+}
