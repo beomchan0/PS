fea39c181bdbcdb97d7bd25f5027e5b8edc12d2e
Beomchan Kim <goldensnitch6750@gmail.com>
2026-07-03 16:36:12 +0900

    [Passed] 별표 출력하기 4 (2ms, 0MB)

---
+++ trail1/별표 출력하기 4/print-star-4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N;
+    cin >> N;
+
+    for(int i=N; i>=1; i--){
+        for(int j=0; j<i; j++){
+            cout << "* "; 
+        }
+        cout << "\n";
+    }
+
+    for(int i=2; i<=N; i++){
+        for(int j=0; j<i; j++){
+            cout << "* ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
