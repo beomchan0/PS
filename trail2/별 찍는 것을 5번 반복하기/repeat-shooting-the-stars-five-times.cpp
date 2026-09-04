48d85973ce09006196a29d16287771fde204c18f
Beomchan Kim <goldensnitch6750@gmail.com>
2026-09-04 21:33:29 +0900

    [Passed] 별 찍는 것을 5번 반복하기 (2ms, 0MB)

---
+++ trail2/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void print10star(){
+    cout <<"**********";
+}
+
+int main() {
+    // Please write your code here.
+    for(int i=0; i<5; i++){
+        print10star();
+        cout<<"\n";
+    }
+    return 0;
+}
