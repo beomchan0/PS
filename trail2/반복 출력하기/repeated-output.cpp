a0ccf49aa30809da7901ada9d732e301a4373120
Beomchan Kim <goldensnitch6750@gmail.com>
2026-09-07 15:19:58 +0900

    [Passed] 반복 출력하기 (2ms, 0MB)

---
+++ trail2/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+void printn(int n){
+    for(int i=0; i<n; i++){
+        cout << "12345^&*()_\n";
+    }
+}
+
+int N;
+
+int main() {
+    cin >> N;
+    printn(N);
+
+    // Please write your code here.
+
+    return 0;
+}
