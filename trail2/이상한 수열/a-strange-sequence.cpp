4a5fb0913f5d6296009e0315c6e86946250b8f90
Beomchan Kim <goldensnitch6750@gmail.com>
2026-09-09 21:10:39 +0900

    [Passed] 이상한 수열 (2ms, 0MB)

---
+++ trail2/이상한 수열/a-strange-sequence.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int f(int n){
+    if(n==1){
+        return 1;
+    }
+    if(n==2){
+        return 2;
+    }
+    return f(n/3)+f(n-1);
+}
+
+int main() {
+    cin >> N;
+
+    cout << f(N);
+
+    // Please write your code here.
+
+    return 0;
+}
