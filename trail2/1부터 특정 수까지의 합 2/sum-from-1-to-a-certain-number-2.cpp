9a84a48ceead9e3ebc8c3472e7421f699c274163
Beomchan Kim <goldensnitch6750@gmail.com>
2026-09-09 20:40:31 +0900

    [Passed] 1부터 특정 수까지의 합 2 (2ms, 0MB)

---
+++ trail2/1부터 특정 수까지의 합 2/sum-from-1-to-a-certain-number-2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int f(int n){
+    if(n==1) return 1;
+
+    return n+f(n-1);
+}
+
+int main() {
+    cin >> N;
+
+    cout << f(N);
+
+    // Please write your code here.
+
+    return 0;
+}
