410111b1b6172e340d2bc57de31a990734e28d95
Beomchan Kim <goldensnitch6750@gmail.com>
2026-09-20 22:28:52 +0900

    [Passed] 정수 명령 처리 3 (4ms, 1MB)

---
+++ trail3/정수 명령 처리 3/process-numeric-commands-3.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <deque>
+
+using namespace std;
+
+int N;
+string cmd[10000];
+int num[10000];
+
+int main() {
+    cin >> N;
+
+    deque<int> d;
+    for (int i = 0; i < N; i++) {
+        cin >> cmd[i];
+        if (cmd[i] == "push_front" || cmd[i] == "push_back") {
+            cin >> num[i];
+
+            if(cmd[i] == "push_front"){
+                d.push_front(num[i]);
+            }else{
+                d.push_back(num[i]);
+            }
+        }else if(cmd[i] == "pop_front"){
+            cout << d.front() << "\n";
+            d.pop_front();
+        }else if(cmd[i] == "pop_back"){
+            cout << d.back() << "\n";
+            d.pop_back();
+        }else if(cmd[i]=="size"){
+            cout << d.size() << "\n";
+        }else if(cmd[i] == "empty"){
+            if(d.empty()){
+                cout << "1\n";
+            }else{
+                cout << "0\n";
+            }
+        }else if(cmd[i] == "front"){
+            cout << d.front() << "\n";
+        }else if(cmd[i] == "back"){
+            cout << d.back() << "\n";
+        }
+    }
+
+    // Please write your code here.
+
+    return 0;
+}
