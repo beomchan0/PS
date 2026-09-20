72828a784c88db498613ad94e65b97d669325c01
Beomchan Kim <goldensnitch6750@gmail.com>
2026-09-20 19:28:44 +0900

    [Passed] 정수 명령 처리 2 (22ms, 1MB)

---
+++ trail3/정수 명령 처리 2/process-numeric-commands-2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+int N;
+string command[10000];
+int A[10000];
+
+int main() {
+    cin >> N;
+
+    queue<int> q;
+
+    for (int i = 0; i < N; i++) {
+        cin >> command[i];
+        if (command[i] == "push") {
+            cin >> A[i];
+            q.push(A[i]);
+        }else if(command[i]=="pop"){
+            cout << q.front() << "\n";
+            q.pop();
+        }else if(command[i]=="size"){
+            cout << q.size() << "\n";
+        }else if(command[i]=="empty"){
+            if(q.empty()){
+                cout << "1\n";
+
+            }else{
+                cout << "0\n";
+            }
+        }else if(command[i]=="front"){
+            cout << q.front() << "\n";
+        }
+    }
+
+    // Please write your code here.
+
+    return 0;
+}
